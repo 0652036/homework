d641434500b5a06e1ac0dfb20089b779fc5919f5
0652036 <abc3117946@gmail.com>
2018-05-18 00:39:20 +0800

    hw

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int HomeWork,Sum;
+int HomeWork,Sum;/*變數宣告*/ 
 int main(int argc, char *argv[]) {
-	for(HomeWork=0;HomeWork<10;HomeWork++){
-		Sum=Sum+HomeWork;
+	for(HomeWork=0;HomeWork<10;HomeWork++)/*從0到9的迴圈*/ 
+	{
+		Sum=Sum+HomeWork;/*計算和*/ 
 	}
-	printf("Sum=%d",Sum);
+	printf("Sum=%d",Sum);/*印出和*/ 
 	return 0;
 }
